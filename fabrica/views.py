b6fff9eb1f088dec9db28d2ac6f81d647a6e6eef
Germán Furfori <german.furfori@gmail.com>
2022-03-19 15:42:08 -0300

    Hice los 2 formularios de carga restantes, con sus respectivas pruebas

--- fabrica/views.py
+++ fabrica/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-from fabrica.forms import IngresarTrabajador
-from fabrica.models import Trabajador
+from fabrica.forms import IngresarMaquina, IngresarProducto, IngresarTrabajador
+from fabrica.models import Maquina, Producto, Trabajador
 
 def inicio(request):
     return render(request, 'fabrica/index.html', {})
@@ -17,14 +17,30 @@ def ingresar_trabajador(request):
             
     formulario = IngresarTrabajador()
     
-    return render(request, 'fabrica/ingresar_trabajador.html', {"formulario": formulario})
+    return render(request, 'fabrica/ingresar.html', {"formulario": formulario})
 
 def ingresar_maquina(request):
+    if request.method == 'POST':
+        formulario = IngresarMaquina(request.POST)
+        
+        if formulario.is_valid():
+            data = formulario.cleaned_data
+            nuevo_curso = Maquina(tipo_de_maquina=data['tipo_de_maquina'], marca=data['marca'])
+            nuevo_curso.save()
+            
+    formulario = IngresarMaquina()
     
-    
-    return render(request, 'fabrica/ingresar_maquina.html', {})
+    return render(request, 'fabrica/ingresar.html', {"formulario": formulario})
 
 def ingresar_producto(request):
+    if request.method == 'POST':
+        formulario = IngresarProducto(request.POST)
+        
+        if formulario.is_valid():
+            data = formulario.cleaned_data
+            nuevo_curso = Producto(producto=data['producto'], cantidad_en_stock=data['cantidad_en_stock'])
+            nuevo_curso.save()
+            
+    formulario = IngresarProducto()
     
-    
-    return render(request, 'fabrica/ingresar_producto.html', {})
+    return render(request, 'fabrica/ingresar.html', {"formulario": formulario})
