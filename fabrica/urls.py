f7e5acd4b63eeea4ee6a975bd19433674bbfa418
Germán Furfori <german.furfori@gmail.com>
2022-03-19 13:01:23 -0300

    Configuré el nav de la plantilla principal, e hice una prueba de herencia

--- fabrica/urls.py
+++ fabrica/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import inicio
+from .views import ingresar_trabajador, inicio
 
 urlpatterns = [
-    path('', inicio, name='inicio')
+    path('', inicio, name='inicio'),
+    path('ingresar_trabajador', ingresar_trabajador, name='ingresar_trabajador')
 ]
--- fabrica/views.py
+++ fabrica/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def inicio(request):
     return render(request, 'fabrica/index.html', {})
+
+def ingresar_trabajador(request):
+    
+    
+    return render(request, 'fabrica/ingresar_trabajador.html', {})
